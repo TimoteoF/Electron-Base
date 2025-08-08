c5c4a844a8df3258f87752a98d0ab9d767791530
Timoteo Ferreri <217746240+TimoteoF@users.noreply.github.com>
2025-08-08 13:16:06 +0200

    Added Tailwind V4

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'node:path';
 import electron from 'vite-plugin-electron/simple';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig(() => ({
     plugins: [
         react(),
+        tailwindcss(),
         electron({
             main: {
                 entry: 'electron/main.ts',
